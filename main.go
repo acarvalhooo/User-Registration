fd961504bf711901ab75bc081b51e7a860b3ddfd
Alef Paulo Barbosa de Carvalho <datarain.alef@intelbras.com.br>
2023-10-31 00:43:43 -0300

    Adding contact infos

--- main.go
+++ main.go
@@ -13,6 +13,34 @@ func main() {
 	fmt.Print("Enter your last name: ")
 	fmt.Scanln(&lastname)
 
-	// Printing full name of user
-	fmt.Println("Your full name is " + name + " " + lastname)
+	// Formating output
+	nameoutput := fmt.Sprintf("Your full name is %s %s", name, lastname)
+
+	// Printing full name
+	fmt.Println("")
+	fmt.Println(nameoutput)
+	fmt.Println("")
+
+	// Invoking function "contact"
+	contact()
+}
+
+func contact() {
+	// Creating variable "email" as string and obtaining value of user input
+	var email string
+	fmt.Print("Enter your e-mail: ")
+	fmt.Scanln(&email)
+
+	// Creating variable "cellphone" as int and obtaining value of user input
+	var cellphone int
+	fmt.Print("Enter your cellphone number, including your country code: +")
+	fmt.Scanln(&cellphone)
+
+	// Formating output
+	contactoutput := fmt.Sprintf("Your e-mail is %s and your cellphone number is +%d", email, cellphone)
+
+	// Printing contact methods
+	fmt.Println()
+	fmt.Println(contactoutput)
+	fmt.Println()
 }
